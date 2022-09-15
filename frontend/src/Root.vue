b4a26b0f2c3a03dbaf601b82a14ca264c797a1ff
kjirou <sorenariblog@gmail.com>
2022-09-15 23:27:42 +0900

    Pass controller_path and action_name into the root component of Vue

--- frontend/src/Root.vue
+++ frontend/src/Root.vue
@@ -4,6 +4,10 @@ import { defineComponent } from "vue";
 import Foo from "./Foo.vue";
 
 export default defineComponent({
+  props: {
+    actionName: { type: String, required: true },
+    controllerPath: { type: String, required: true },
+  },
   components: {
     foo: Foo,
   },
@@ -12,6 +16,10 @@ export default defineComponent({
 
 <template>
   <h1>Rails Vue MPA</h1>
+  <ul>
+    <li>controllerPath = {{ controllerPath }}</li>
+    <li>actionName = {{ actionName }}</li>
+  </ul>
   <foo
     :a="'a'"
     :b="'b'"
--- frontend/src/index.ts
+++ frontend/src/index.ts
@@ -2,8 +2,38 @@ import { createApp } from "vue";
 
 import Root from "./Root.vue";
 
-const app = createApp(Root);
+/**
+ * @todo Extracts and returns initial props.
+ */
+const extractDataFromRails = (
+  vueRootElement: HTMLElement
+): {
+  actionName: string;
+  controllerPath: string;
+} => {
+  const controllerPath = vueRootElement.getAttribute("data-controller-path");
+  const actionName = vueRootElement.getAttribute("data-action-name");
+  if (controllerPath !== null && actionName !== null) {
+    return {
+      controllerPath,
+      actionName,
+    };
+  }
+  throw new Error(
+    "data-controller-path and data-action-name are not set on the root element of Vue."
+  );
+};
 
 document.addEventListener("DOMContentLoaded", () => {
-  app.mount("#vue-root");
+  const vueRootElement = document.querySelector<HTMLElement>("#vue-root");
+  if (vueRootElement === null) {
+    throw new Error("Can not find the destination of Vue's `app.mount()`.");
+  }
+  const { controllerPath, actionName } = extractDataFromRails(vueRootElement);
+  // TODO: The following root props are not typed.
+  const app = createApp(Root, {
+    controllerPath,
+    actionName,
+  });
+  app.mount(vueRootElement);
 });
