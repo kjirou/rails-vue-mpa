42909d68babddabb7b1736db9e5e471c2f29bb63
kjirou <sorenariblog@gmail.com>
2022-09-20 09:31:48 +0900

    Add a comment

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   private
 
   def render_frontend(page_data: {})
+    # TODO: Varidate that the `page_data` is schema compliant.
     render html: '', layout: true, assigns: { page_data: page_data }
   end
 end
